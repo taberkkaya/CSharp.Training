015a8f14d0a5677e1715ca9e52e53e2d6e1736e7
taberkkaya <taberkkaya@gmail.com>
2025-02-05 21:29:29 +0300

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Encapsulation
+{
+    internal class Program : User
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Id = 1;
+
+            Program program = new();
+            program.Age = 1;
+        }
+    }
+}
---
+++ Encapsulation/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class User
+    {
+        public int Id { get; set; }
+        private string Name { get; set; }
+        protected int Age { get; set; }
+    }
+}
