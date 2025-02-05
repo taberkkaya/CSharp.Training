4710eb4b9b2e499f490d84a0f28f6fafeec53b2f
taberkkaya <taberkkaya@gmail.com>
2025-02-05 21:35:41 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User2 user = new();
+            user.SetAge();
+            var mail = user.Mail;
+            user.Set();
+        }
+    }
+}
---
+++ Inheritance/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class User : Test1
+    {
+        public int Id { get; set; }
+        public int SetAge() {
+            return 0;
+        }
+    }
+
+    public class User2 : User
+    {
+        
+    }
+
+    public class Test1 : Test2 
+    {
+        public string Mail { get; set; }
+    }
+
+    public class Test2
+    {
+        public int Set()
+        {
+            return 0;
+        }
+    }
+}
