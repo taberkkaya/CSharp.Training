ce6cfd292d5b2fd7d6421c37d8457b3d825ca783
taberkkaya <taberkkaya@gmail.com>
2025-02-05 21:57:00 +0300

    Ref

---
+++ Ref/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ref
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            int age = 5;
+            Console.WriteLine(age);
+            user.SetAge(ref age);
+            Console.WriteLine(age);
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ref/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref
+{
+    public class User
+    {
+        public void SetAge(ref int age)
+        {
+            age += 10;
+        }
+    }
+}
