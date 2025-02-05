47fb29a189d992b6402646e2810e38287d73793f
taberkkaya <taberkkaya@gmail.com>
2025-02-05 21:53:09 +0300

    Advanced Methods #Params

---
+++ Params/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            User user = new User();
+            Console.WriteLine(user.Calculate(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        }
+    }
+}
---
+++ Params/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    public class User
+    {
+        public int Calculate(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
