e61d1e41ef70ca3402fee0c8c420558fda7b102a
taberkkaya <taberkkaya@gmail.com>
2025-02-05 21:44:39 +0300

    Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new();
+            Console.WriteLine(user.GetAge());
+            
+            User2 user2 = new();
+            Console.WriteLine(user2.GetAge());
+        }
+    }
+}
---
+++ Polymorphism/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class User
+    {
+        public int SetAge()
+        {
+            return 1;
+        }
+
+        public int SetAge(int age)
+        {
+            return age;
+        }
+
+        public virtual int GetAge()
+        {
+            return 1;
+        }
+    }
+
+    public class User2 : User
+    {
+        public override int GetAge()
+        {
+            return 15;
+        }
+    }
+}
