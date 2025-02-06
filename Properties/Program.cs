08affe460e49927f6b8039d721faa37b04053786
taberkkaya <taberkkaya@gmail.com>
2025-02-07 01:03:51 +0300

    Properties

---
+++ Properties/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Id = 1;
+            //user.Name = "Ataberk Kaya";
+            string name = user.Name;
+
+            user.Properties = "Prop";
+
+            Console.WriteLine(user.Properties);
+        }
+    }
+}
---
+++ Properties/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class User
+    {
+        private string _name;
+        public int Id { get; set; }
+        public string Name { get;}
+
+        public string Properties { 
+            get { return _name; } 
+            set { _name = value + " lorem"; } 
+        }
+
+        private string _test;
+        public string Test {
+            get => _test;
+            set => _test = value;
+        }
+
+        private string _firstName;
+        private string _lastName;
+
+        public User()
+        {
+            
+        }
+
+        public User(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;   
+        }
+
+        public string FullName => $"{_firstName} {_lastName}";
+
+            
+
+    }
+}
