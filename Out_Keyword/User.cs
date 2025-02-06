85be0ae15013f27dbdb7152086693d8b9932248f
taberkkaya <taberkkaya@gmail.com>
2025-02-06 03:55:35 +0300

    Out Keyword

---
+++ Out_Keyword/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_Keyword
+{
+    public class User
+    {
+        public void SetAge(ref int age)
+        {
+            age += 10;
+        }
+        public void SetAge2(out int age)
+        {
+            age = 10;
+        }
+    }
+}
