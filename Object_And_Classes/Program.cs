0bf7337740bb912e5f0eebf417d3b3d99a6aad27
taberkkaya <taberkkaya@gmail.com>
2025-02-06 03:58:21 +0300

    Object and classes

---
+++ Object_And_Classes/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Object_And_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            User user = new User(); //object
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
