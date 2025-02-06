4ceaa8a32e1a2881db06756d2745a12f7333eb27
taberkkaya <taberkkaya@gmail.com>
2025-02-06 04:21:54 +0300

    Member

---
+++ Member/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Member
+{
+    public class User
+    {
+        public int Id { get; set; } //prop member
+        public string Name; //field member
+
+        public void Print() //method
+        {
+                
+        }
+    }
+}
