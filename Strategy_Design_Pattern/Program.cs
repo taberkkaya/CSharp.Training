98298f5b5aad5da179399ab12d5e44244a110885
taberkkaya <taberkkaya@gmail.com>
2025-02-07 17:00:33 +0300

    Strategy Design Pattern

---
+++ Strategy_Design_Pattern/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Strategy_Design_Pattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            OzelYapimUret ozel = new();
+            SeriUret seriUret = new();
+            SiparisUzerine siparisUzerine = new();
+
+            Uretici uretici = new(ozel);
+
+        }
+
+
+
+
+        public class Opel
+        {
+            public Opel(string uretimTipi)
+            {
+                Console.WriteLine($"Opel {uretimTipi} üretilmiştir.");
+            }
+
+        }
+
+        abstract class Startegy
+        {
+            public abstract Opel Uret();
+        }
+
+        class SeriUret : Startegy
+        {
+            public override Opel Uret()
+            {
+                return new Opel("Seri");
+            }
+        }
+
+        class OzelYapimUret : Startegy
+        {
+            public override Opel Uret()
+            {
+                return new Opel("Özel");
+            }
+        }
+
+        class SiparisUzerine : Startegy
+        {
+            public override Opel Uret()
+            {
+                return new Opel("Sipariş üzerine");
+            }
+        }
+
+        class Uretici
+        {
+            public Uretici(Startegy yontem)
+            {
+                yontem.Uret();
+            }
+        }
+
+    }
+}
