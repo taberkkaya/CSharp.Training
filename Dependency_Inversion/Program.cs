6abdfda06e7b4ad5c7a43ddeae02bdc9e03dfd15
taberkkaya <taberkkaya@gmail.com>
2025-02-07 04:14:47 +0300

    Solid #Dependency Inversion

---
+++ Dependency_Inversion/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Dependency_Inversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ILogger logger = new DbLog();
+            ILogger logger = new TextLog();
+            logger.Log();
+        }
+
+        interface ILogger
+        {
+            void Log();
+        }
+
+        public class DbLog : ILogger
+        {
+            public void Log()
+            {
+                Console.WriteLine("DbLogger");
+            }
+        }
+
+        public class TextLog : ILogger
+        {
+            public void Log()
+            {
+                Console.WriteLine("TextLogger");
+            }
+        }
+    }
+}
