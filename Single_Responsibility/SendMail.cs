05f8c9b20021a46c0e287572cbc98fecd5a4e723
taberkkaya <taberkkaya@gmail.com>
2025-02-07 03:51:51 +0300

    Solid #Single Responsibility

---
+++ Single_Responsibility/SendMail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility
+{
+    public class SendMail
+    {
+        public void GmailSendMail(string subject, string body, string email)
+        {
+            EmailAddressValidator.IsEmailValid(email);
+        }
+
+    }
+
+    public static class EmailAddressValidator
+    {
+        public static void IsEmailValid(string email)
+        {
+            //control
+            //throw new Exception("Email is not valid.");
+        }
+    }
+}
