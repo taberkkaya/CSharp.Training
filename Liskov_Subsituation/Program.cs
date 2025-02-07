b2a3f5a1211110a2dca9860b92c1a0fb54d62ec1
taberkkaya <taberkkaya@gmail.com>
2025-02-07 04:05:28 +0300

    Solid #Liskov Subsituation

---
+++ Liskov_Subsituation/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Liskov_Subsituation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HP hP = new HP();
+            hP.Print();
+            hP.Scan();
+
+            Canon canon = new Canon();
+            canon.Print();
+            //canon.Scan();
+
+        }
+
+        public abstract class BasePrinter
+        {
+            public abstract void Print();
+        }
+
+        public interface IScanner
+        {
+            void Scan();
+        }
+
+        public class HP : BasePrinter, IScanner
+        {
+            public override void Print()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class Canon : BasePrinter
+        {
+            public override void Print()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
