ecdc306332b7a40c3f214893d204a20db4dfebd3
taberkkaya <taberkkaya@gmail.com>
2025-02-07 04:09:31 +0300

    Solid #Interface Segregation

---
+++ Interface_Segregation/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Interface_Segregation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        interface IGet
+        {
+            void Get();
+
+        }
+
+        interface IApiMethods : IGet
+        {
+            void Post();
+        }
+
+        class News : IApiMethods
+        {
+            public void Get()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Post()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class Notification : IGet
+        {
+            public void Get()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
