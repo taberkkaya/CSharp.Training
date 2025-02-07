6b1e2f452187ff7798782a432b5ee01c4335807e
taberkkaya <taberkkaya@gmail.com>
2025-02-07 04:00:02 +0300

    Solid #Open Closed

---
+++ Open_Closed/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Open_Closed
+{
+    internal class Program
+    {
+        private readonly ILog _log;
+
+        public Program(ILog log)
+        {
+            _log = log;
+        }
+
+        //static void Main(string[] args, LogType type)
+        void Main(string[] args)
+        {
+            //if (type == LogType.DbLog)
+            //{
+            //    DbLog dbLog = new DbLog();
+            //    dbLog.Log("");
+            //}else if(type == LogType.XMLLog)
+            //{
+            //    XMLLog xmlLog = new XMLLog();
+            //    xmlLog.Log("");
+            //}
+
+            //ILog _log = new XMLLog();
+            _log.Log("");
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        public interface ILog
+        {
+            void Log(string message);
+        }
+
+        public class DbLog : ILog
+        {
+            public void Log(string value)
+            {
+
+            }
+        }
+
+        public class XMLLog : ILog
+        {
+            public void Log(string value)
+            {
+
+            }
+        }
+
+        public class txtLog : ILog
+        {
+            public void Log(string message)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        //public enum LogType
+        //{
+        //    DbLog,
+        //    XMLLog
+        //}
+    }
+}
